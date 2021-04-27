dc84d1dbd979162ca983e39ffde97519835e84dc
Nyheim Hunter <nyheimhunter@Nyheims-Air.attlocal.net>
2021-04-26 19:00:03 -0700

    Added Auth controller & token data to Application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
 
-  # GET /users
-  def index
-    @users = User.all
-
-    render json: @users
-  end
-
-  # GET /users/1
-  def show
-    render json: @user
-  end
 
   # POST /users
   def create
@@ -37,18 +25,8 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-
+    
     # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:username, :name, :email, :password)
