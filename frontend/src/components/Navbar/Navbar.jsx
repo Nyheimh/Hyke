2b5c55048465cfb19c13820de0b69a108e22aac3
Nyheim Hunter <nyheimhunter@Nyheims-MacBook-Air.local>
2021-04-30 16:09:38 -0700

    Navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -7,12 +7,10 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <div className="navbar">
-      <Header />
+      {/* <Header /> */}
       <div className="links">
         <Link to="/"> Home </Link>
         <Link to="/trails">Hykes</Link>
-      {/* </div> */}
-      {/* <div className="home__headerRight"> */}
         <Link to="/trails/new">Add Hyke</Link>
         <Link to="/about">About</Link>
       </div>
