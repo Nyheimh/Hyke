44aeb9c45f7fd22ff0c5de9f15a75b0a1f05b0dc
Nyheim Hunter <nyheimhunter@Nyheims-MacBook-Air.local>
2021-04-30 07:57:15 -0700

    Header

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
+
+const authenticatedOptions = (
+  <>
+    {/* <NavLink to="/watchlist">
+      Watchlist
+    </NavLink> */}
+    <NavLink id="appName" to="/">
+      Hyke
+    </NavLink>
+    <NavLink id="add-trail" to="/trails/new">
+      Add Hyke
+    </NavLink>
+    <NavLink id="sign-out" to="/sign-out">
+      Log Out
+    </NavLink>
+  </>
+);
+
+const unauthenticatedOptions = (
+  <>
+    <NavLink id="register" to="/register">
+      Register
+    </NavLink>
+    <NavLink id="log-in" to="/login">
+      Log In
+    </NavLink>
+  </>
+);
+
+const alwaysOptions = (
+  <div id="alwaysOptions">
+    <NavLink className="home" to="/">
+      Home
+    </NavLink>
+    <NavLink className="all-trails" to="/trails">
+      All Trails
+    </NavLink>
+    <NavLink className="about" to="/about">
+      About
+    </NavLink>
+  </div>
+);
+
+export default function Header() {
+  return (
+    <nav>
+      <div className="nav">
+        <div className="links">
+          <div className="authorized">
+            {user && (
+              <div className="userWelcome">
+                <div></div>
+                {/* Welcome, {username} */}
+              </div>
+            )}
+            <div className="navOptions">
+              {alwaysOptions}
+              {user ? authenticatedOptions : unauthenticatedOptions}
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,22 @@
 import "./Navbar.css";
+import Header from "../Header/Header"
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div className="navbar">
-      {/* <div className="home__headerLeft"> */}
-        <Link to="/"> Home </Link>
-        <Link to="/trails">Hykes</Link>
-      {/* </div> */}
-      {/* <div className="home__headerRight"> */}
-        <Link to="/trails/new">Add Hyke</Link>
-        <Link to="/about">About</Link>
-        {/* </div> */}
+      <Header />
+      
     </div>
   );
 }
+
+// {/* <div className="home__headerLeft"> */}
+//         <Link to="/"> Home </Link>
+//         <Link to="/trails">Hykes</Link>
+//       {/* </div> */}
+//       {/* <div className="home__headerRight"> */}
+//         <Link to="/trails/new">Add Hyke</Link>
+//         <Link to="/about">About</Link>
+//       {/* </div>
+//        */}
