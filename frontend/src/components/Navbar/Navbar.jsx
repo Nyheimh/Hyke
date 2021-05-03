2d473dca176b7a9b71984a7c6ef56db47bccfd6d
Nyheim Hunter <nyheimhunter@Nyheims-Air.attlocal.net>
2021-05-02 18:43:37 -0700

    Index update

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,33 +1,27 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 
-
-
 export default function Navbar(props) {
+  const { currentUser, handleLogout } = props;
 
-
-  const { currentUser, handleLogout } = props
-  
   return (
     <div className="navbar">
       <div className="links">
         <Link to="/"> Home </Link>
         <Link to="/trails">Hykes</Link>
-        {currentUser &&
-        <Link to="/trails/new">Add Hyke</Link>}
+        {currentUser && <Link to="/trails/new">Add Hyke</Link>}
         <Link to="/about">About</Link>
-      {currentUser ? (
-        <div className="username">
-          <p className="welcome">Welcome, {currentUser.username}</p>
-          <section className="navbar-button">
-
-            <button onClick={handleLogout}>Logout</button>
-          </section>
+        {currentUser ? (
+          <div className="username">
+            <p className="welcome">Welcome, {currentUser.username}</p>
+            <section className="navbar-button">
+              <button onClick={handleLogout}>Logout</button>
+            </section>
           </div>
         ) : (
           <Link to="/login">Login/Register</Link>
-          )}
-          </div>
+        )}
+      </div>
     </div>
   );
 }
