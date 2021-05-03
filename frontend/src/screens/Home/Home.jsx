17bd8e89c299fd1f68bdebfa73167529c17c5f7e
Nyheim Hunter <nyheimhunter@Nyheims-Air.attlocal.net>
2021-05-02 18:49:59 -0700

    Home updated

--- frontend/src/screens/Home/Home.jsx
+++ frontend/src/screens/Home/Home.jsx
@@ -1,19 +1,17 @@
 import "./Home.css";
 import React from "react";
-import { Link } from "react-router-dom";
-import Search from '../../components/Search/Search'
-
+import Search from "../../components/Search/Search";
 
 function Home() {
   return (
     <div className="home">
       <div className="home-body">
         <h1>Hyke</h1>
-        <div className="home__inputContainer">
-        <Search />
+        <div className="home-inputContainer">
+          <Search />
         </div>
       </div>
-      </div>
+    </div>
   );
 }
 
